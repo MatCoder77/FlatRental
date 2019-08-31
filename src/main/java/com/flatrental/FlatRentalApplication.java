54ee8a929a199b427bcc88129cbb9982c52f93da
MatCoder77 <matkimzag@wp.pl>
2019-08-31 14:36:07 +0200

    working startup

--- src/main/java/com/flatrental/FlatRentalApplication.java
+++ src/main/java/com/flatrental/FlatRentalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlatrentalApplication {
+public class FlatRentalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FlatrentalApplication.class, args);
+		SpringApplication.run(FlatRentalApplication.class, args);
 	}
 
 }
--- src/main/java/com/flatrental/ServletInitializer.java
+++ src/main/java/com/flatrental/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FlatrentalApplication.class);
+		return application.sources(FlatRentalApplication.class);
 	}
 
 }
